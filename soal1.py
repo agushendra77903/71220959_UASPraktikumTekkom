d022ccb0db6f2d91bb93beeebfeb908039a77551
agushendra77903 <117900786+agushendra77903@users.noreply.github.com>
2023-01-03 14:57:11 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,24 @@
+print("====================================================== \n ******************Justice League******************** \n ======================================================")
+ept= ['brucewyne''victorstone''ciscoramon']
+int=input("masukkan username anda: ")
+
+print("====== WELCOME",(int),"=======")
+print("1.tambaha anggota")
+print("2.hapus anggota justice league")
+print("3.tampilkan anggota justice league")
+print("4.exit")
+akun=input("masukkan pilihan anda: ")
+if akun == "1":
+    eks=input("Nama anggota baru:")
+    print("data",(eks),"berhasil di tambahkan")
+if akun == "2":
+    eps=input("anggota yang akan di hapus:")
+    print("data",(eps),"berhasil di hapus")
+if akun == "3":
+    print("daftar anggota Justice League:")
+    print("|Barry Allen| |Diana Prince| |Arthur Curry|")
+if akun == "4":
+    print("see u next time MR.",(akun),",GLHF >>>>>")
+
+
+
