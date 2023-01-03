2c6314eed61dd04fe6874b591ab89d689f1a7d32
agushendra77903 <117900786+agushendra77903@users.noreply.github.com>
2023-01-03 14:57:35 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,24 @@
+print("==== selamat datang di XXV ====")
+nama= input("masukkan tanggal hari ini:")
+
+print("== berikut genre film yang tersedia ==")
+print("1. horor")
+print("2. action")
+pilihan= input ("silahkan pilih mau nonton film bergenre apa:")
+if pilihan == "1":
+    print("== berikut pilihan film horor yang sedang tayang ==")
+    print("1. The Devil's Light")
+    print("2. pengabdi setan")
+    akun= input("silahkan mau pilih nonton film apa:")
+    ent= input("mau memesan tiket sebanyak:")
+elif pilihan == "2":
+    print("== berikut pilihan film horor yang sedang tayang ==")
+    print("1.Black Panther: Wakanda Forefer")
+    print("2.Avatar: The Way of Water")
+    akun= input("silahkan mau pilih nonton film apa:")
+    ent= input("mau memesan tiket sebanyak:")
+
+else:
+    ("pilihan yang anda pilih tidakk tersedia di bioskop ini")
+
+
